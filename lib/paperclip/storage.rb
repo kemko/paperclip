a896130318aea9fe4e29ac054e6f31f75bccf38b
Max Melentiev <melentievm@gmail.com>
2018-11-09 16:18:49 +0300

    Add S3 storage from upstream master

--- lib/paperclip/storage.rb
+++ lib/paperclip/storage.rb
@@ -2,5 +2,6 @@ module Paperclip
   module Storage
     autoload :Filesystem, 'paperclip/storage/filesystem'
     autoload :Delayeds3, 'paperclip/storage/delayeds3'
+    autoload :S3, 'paperclip/storage/s3'
   end
 end
