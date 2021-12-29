959b16119104420ff5d202288e3fee6705c0c350
Nastia Gorokhova-Alekseeva <ngoral@304.ru>
2021-12-29 21:08:19 +0300

    Восстанавливаем прохождение тестов в iostream_test

--- test/iostream_test.rb
+++ test/iostream_test.rb
@@ -1,61 +1,21 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class IOStreamTest < Test::Unit::TestCase
-  context "IOStream" do
-    should "be included in IO, File, Tempfile, and StringIO" do
-      [IO, File, Tempfile, StringIO].each do |klass|
-        assert klass.included_modules.include?(IOStream), "Not in #{klass}"
-      end
-    end
-  end
-
   context "A file" do
     setup do
+      rebuild_model
       @file = File.new(File.join(File.dirname(__FILE__), "fixtures", "5k.png"), 'rb')
+      @dummy = Dummy.new
+      @dummy.avatar = @file
     end
 
     teardown { @file.close }
 
-    context "that is sent #stream_to" do
-
-      context "and given a String" do
-        setup do
-          FileUtils.mkdir_p(File.join(ROOT, 'tmp'))
-          assert @result = @file.stream_to(File.join(ROOT, 'tmp', 'iostream.string.test'))
-        end
-
-        should "return a File" do
-          assert @result.is_a?(File)
-        end
-
-        should "contain the same data as the original file" do
-          @file.rewind; @result.rewind
-          assert_equal @file.read, @result.read
-        end
-      end
-
-      context "and given a Tempfile" do
-        setup do
-          tempfile = Tempfile.new('iostream.test')
-          tempfile.binmode
-          assert @result = @file.stream_to(tempfile)
-        end
-
-        should "return a Tempfile" do
-          assert @result.is_a?(Tempfile)
-        end
-
-        should "contain the same data as the original file" do
-          @file.rewind; @result.rewind
-          assert_equal @file.read, @result.read
-        end
-      end
-
-    end
-
     context "that is sent #to_tempfile" do
       setup do
-        assert @tempfile = @file.to_tempfile
+        assert @tempfile = @dummy.avatar.to_tempfile(@file)
       end
 
       should "convert it to a Tempfile" do
