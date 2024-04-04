d0354efcc8e5e28ac7a6bb4c7eb798c61d4a6d1a
Vasily Fedoseyev <vasilyfedoseyev@gmail.com>
2024-04-05 00:25:25 +0300

    Delete intermediary tempfiles and tempfiles for styles

--- lib/paperclip/recursive_thumbnail.rb
+++ lib/paperclip/recursive_thumbnail.rb
@@ -5,8 +5,12 @@ module Paperclip
       # если по каким-то причинам не сформировался файл
       # для прыдущего размера не кидаем ексепшен и
       # генерим файл из оригинального
-      f = attachment.to_file(options[:thumbnail] || :original) rescue file
-      super f, options, attachment
+      source_style = options[:thumbnail] || :original
+      # TODO: вообще queued_for_write[source_style] место в NoCacheS3#to_file
+      f = attachment&.queued_for_write&.dig(source_style)&.tap(&:rewind)
+      # TODO: и надо сносить файл если все же была загрузка
+      f ||= attachment.to_file(source_style) rescue file # rubocop:disable Style/RescueModifier
+      super(f, options, attachment)
     end
   end
 end
