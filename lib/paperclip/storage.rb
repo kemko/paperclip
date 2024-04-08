06e4d703168399ef7786fb9705bd6187a654a94a
Vasily Fedoseyev <vasilyfedoseyev@gmail.com>
2024-04-08 22:17:10 +0300

    Remove delayeds3 storage

--- lib/paperclip/storage.rb
+++ lib/paperclip/storage.rb
@@ -2,7 +2,6 @@ module Paperclip
   module Storage
     autoload :Filesystem, 'paperclip/storage/filesystem'
     autoload :DelayedUpload, 'paperclip/storage/delayed_upload'
-    autoload :Delayeds3, 'paperclip/storage/delayeds3'
     autoload :Cached, 'paperclip/storage/cached'
     autoload :NoCacheS3, 'paperclip/storage/no_cache_s3'
   end
--- test/storage_test.rb
+++ test/storage_test.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'aws-sdk-s3'
 
 class StorageTest < Test::Unit::TestCase
-  context 'An attachment with Delayeds3 storage' do
+  context 'An attachment with no_cache_s3 storage' do
     setup do
-      rebuild_model storage: :delayeds3,
-                    bucket: 'testing',
-                    path: ':attachment/:style/:basename.:extension',
-                    s3_credentials: {},
-                    yandex_credentials: {},
-                    sbercloud_credentials: {}
+      rebuild_model storage: :no_cache_s3,
+                    key: ':filename',
+                    stores: {
+                      store1: { access_key_id: '123', secret_access_key: '123', region: 'r', bucket: 'buck' }
+                    }
     end
 
-    should 'be extended by the Delayeds3 module' do
-      assert Dummy.new.avatar.is_a?(Paperclip::Storage::Delayeds3)
+    should 'be extended by the NoCacheS3 module' do
+      assert Dummy.new.avatar.is_a?(Paperclip::Storage::NoCacheS3)
     end
 
     should 'not be extended by the Filesystem module' do
