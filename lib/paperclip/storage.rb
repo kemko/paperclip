d122e78c0ed42cde8f1e4aaabb33b82c50016984
Vasily Fedoseyev <vasilyfedoseyev@gmail.com>
2024-04-08 22:19:15 +0300

    Remove cached storage

--- lib/paperclip/storage.rb
+++ lib/paperclip/storage.rb
@@ -2,7 +2,6 @@ module Paperclip
   module Storage
     autoload :Filesystem, 'paperclip/storage/filesystem'
     autoload :DelayedUpload, 'paperclip/storage/delayed_upload'
-    autoload :Cached, 'paperclip/storage/cached'
     autoload :NoCacheS3, 'paperclip/storage/no_cache_s3'
   end
 end
